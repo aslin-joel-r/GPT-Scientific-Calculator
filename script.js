af4b9f85163515a7df6a503d87ce6986d54def6f
aslin-r <aslinjoelr123@gmail.com>
2023-08-08 23:01:46 +0530

    solved the functionality issue in sqrt

--- script.js
+++ script.js
@@ -1,65 +1,47 @@
 function appendToResult(value) {
-    document.getElementById("result").value += value;
-  }
-  
-  function calculate() {
-    try {
-      let result = eval(document.getElementById("result").value);
-      document.getElementById("result").value = result;
-    } catch (error) {
-      document.getElementById("result").value = "Error";
-    }
-  }
-  
-  function clearResult() {
-    document.getElementById("result").value = "";
-  }
-  
-  function deleteLast() {
-    let currentResult = document.getElementById("result").value;
-    document.getElementById("result").value = currentResult.slice(0, -1);
-  }
-  
-  function calculateSquareRoot() {
-    document.getElementById("result").value += "Math.sqrt()";
-  }
-  
-  
-  function calculatePower() {
-    let currentResult = document.getElementById("result").value;
-    let result = Math.pow(parseFloat(currentResult), 2);
-    document.getElementById("result").value = result;
-  }
-  
-  function calculateSin() {
-    let currentResult = document.getElementById("result").value;
-    let result = Math.sin(parseFloat(currentResult));
-    document.getElementById("result").value = result;
-  }
-  
-  function calculateCos() {
-    let currentResult = document.getElementById("result").value;
-    let result = Math.cos(parseFloat(currentResult));
-    document.getElementById("result").value = result;
-  }
-  
-  function calculateTan() {
-    let currentResult = document.getElementById("result").value;
-    let result = Math.tan(parseFloat(currentResult));
-    document.getElementById("result").value = result;
-  }
+  document.getElementById("result").value += value;
+}
 
-  function calculate() {
-    try {
-      let result = eval(document.getElementById("result").value);
-      if (isNaN(result) || result === Infinity || result === -Infinity) {
-        document.getElementById("result").value = "Error";
-      } else {
-        document.getElementById("result").value = result;
-      }
-    } catch (error) {
-      document.getElementById("result").value = "Error";
-    }
+function calculate() {
+  try {
+    let result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
+  } catch (error) {
+    document.getElementById("result").value = "Error";
   }
-  
-  
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function deleteLast() {
+  let currentResult = document.getElementById("result").value;
+  document.getElementById("result").value = currentResult.slice(0, -1);
+}
+
+function calculateSquareRoot() {
+  document.getElementById("result").value += "Math.sqrt(";
+}
+
+function calculatePower() {
+  document.getElementById("result").value += "Math.pow(";
+}
+
+function calculateSin() {
+  let currentResult = document.getElementById("result").value;
+  let result = Math.sin(parseFloat(currentResult));
+  document.getElementById("result").value = result;
+}
+
+function calculateCos() {
+  let currentResult = document.getElementById("result").value;
+  let result = Math.cos(parseFloat(currentResult));
+  document.getElementById("result").value = result;
+}
+
+function calculateTan() {
+  let currentResult = document.getElementById("result").value;
+  let result = Math.tan(parseFloat(currentResult));
+  document.getElementById("result").value = result;
+}
